f31a757169050d05bc5d933b234eabe335438d0d
testEpiDeMic <48357409+testEpiDeMic@users.noreply.github.com>
2019-03-13 01:42:57 +0530

    Update GenderClassifier.py
    Final Python Script with the classifier running on the Decision Tree model

--- GenderClassifier.py
+++ GenderClassifier.py
@@ -1,2 +1,49 @@
 from sklearn import tree
-print("Hello")
+import xlrd
+
+#storing the location of the Excel Workbook containing the Training Samples
+ipWBLoc = ("TrainingSamples.xlsx")
+
+#opening the said input Excel Workbook
+ipWB = xlrd.open_workbook(ipWBLoc)
+
+#opening the sheet containing the Feature Vectors into 'sheetFV'
+sheetNameFV = "FeatureVectors"
+sheetFV = ipWB.sheet_by_name(sheetNameFV)
+#opening the sheet containing the Labels into 'sheetLabels'
+sheetNameLabels = "Labels"
+sheetLabels = ipWB.sheet_by_name(sheetNameLabels)
+
+#storing the no. of Training Samples into noTSFV
+#noTSFV = sheetFV.nrows
+noTS = sheetFV.nrows
+
+#storing the dimensionality of the Training Samples into dimTS
+dimTS = sheetFV.ncols
+
+#storing the no. of Training Sample Labels into noLabels
+#optional test to check if noTS matches with noLabels
+
+#creating a list of Training Sample Feature Vectors
+listTSFV = []
+for TSCounter in range(noTS):
+	TSFV = []
+	for dimCounter in range(dimTS):
+		TSFV.append(sheetFV.cell_value(TSCounter,dimCounter))
+	listTSFV.append(TSFV)
+
+#creating a list of Training Sample Labels (assumming they are in the first column of the worksheet)
+listTSLabels = []
+for TSCounter in range(noTS):
+	listTSLabels.append(sheetLabels.cell_value(TSCounter,0))
+
+#creating a classifier 'clf' with the Descision Tree model
+clf = tree.DecisionTreeClassifier()
+
+#training the classifier with the Decision Tree model
+clf.fit(listTSFV,listTSLabels)
+
+#testing the classifier on 'testInput' (LIST of test Sample FVs: so a list of lists) and printing the output
+testInput = [[190,70,43]]
+print(clf.predict(testInput))
+
